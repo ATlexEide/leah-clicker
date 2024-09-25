538ae6480745d525fdf5ae1ffeb6e583f3b60b76
Alexander Eide <atlexeide@gmail.com>
2024-09-25 20:19:29 +0200

    Add / Update price logic

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,7 @@ const game = {
     addMonster:()=>{
         game.monsterAmount++;
         document.querySelector('#count-amount').textContent=`x ${game.monsterAmount}`
+        
         game.getMonstersLeft()
     },
     // // White Monsters / Points    monsterAmount: undefined,
@@ -17,7 +18,7 @@ const game = {
     // Upgrades
     upgrades:[
         {   
-            name: 'Programmer Socks',
+            name: 'programmerSocks',
             price: 100,
             monstersLeftToBuy: 999,
             multiplier: undefined,
@@ -25,16 +26,16 @@ const game = {
 
         },
         {
-            name: 'Miku',
-            price: 100,
+            name: 'miku',
+            price: 500,
             monstersLeftToBuy: 999,
             multiplier: undefined,
             description:'lorem ipsum',
 
         },
         {
-            name: 'Neeko',
-            price: 100,
+            name: 'neeko',
+            price: 1000,
             monstersLeftToBuy: 999,
             multiplier: undefined,        
             description:'lorem ipsum',
@@ -44,15 +45,13 @@ const game = {
     getMonstersLeft: ()=>{
         for(const upgrade of game.upgrades){
             upgrade.monstersLeftToBuy = upgrade.price - game.monsterAmount
-            console.log(`Monsters left to buy ${upgrade.name}: ${upgrade.monstersLeftToBuy}`)
+            document.getElementById(`${upgrade.name}-price`).textContent = `Price: ${upgrade.price} >> Monsters remaining to buy: ${upgrade.monstersLeftToBuy}`
         }
     }
 }
 document.querySelector('#leah').addEventListener('mousedown',(e)=>{
 e.preventDefault()
+    console.clear()
     game.addClick()
+    console.table(game.upgrades)
 })
-
-
-
-console.log(game.upgrades[0].left)
