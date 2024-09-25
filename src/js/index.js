dd3c96ff077ee161fac747718a64d28e32124bf8
Alexander Eide <atlexeide@gmail.com>
2024-09-25 18:27:44 +0200

    Add game logic

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,58 @@
 const game = {
-    // White Monsters / Points    monsterAmount: undefined,
+    // Add clicks
+    addClick:()=>{
+        game.clicks++;
+        game.addMonster();
+    },
+    // Add monsters
+    addMonster:()=>{
+        game.monsterAmount++;
+        document.querySelector('#count-amount').textContent=`x ${game.monsterAmount}`
+        game.getMonstersLeft()
+    },
+    // // White Monsters / Points    monsterAmount: undefined,
     monsterAmount: 0,
     // Clicks
     clicks: 0,
     // Upgrades
-    upgrades:{
-        programmerSocks:{
+    upgrades:[
+        {   
+            name: 'Programmer Socks',
+            price: 100,
+            monstersLeftToBuy: 999,
             multiplier: undefined,
+            description:'lorem ipsum',
+
         },
-        miku:{
+        {
+            name: 'Miku',
+            price: 100,
+            monstersLeftToBuy: 999,
             multiplier: undefined,
+            description:'lorem ipsum',
+
         },
-        neeko:{
+        {
+            name: 'Neeko',
+            price: 100,
+            monstersLeftToBuy: 999,
             multiplier: undefined,        
+            description:'lorem ipsum',
+
         },
+    ],
+    getMonstersLeft: ()=>{
+        for(const upgrade of game.upgrades){
+            upgrade.monstersLeftToBuy = upgrade.price - game.monsterAmount
+            console.log(`Monsters left to buy ${upgrade.name}: ${upgrade.monstersLeftToBuy}`)
+        }
     }
 }
+document.querySelector('#leah').addEventListener('mousedown',(e)=>{
+e.preventDefault()
+    game.addClick()
+})
+
+
+
+console.log(game.upgrades[0].left)
