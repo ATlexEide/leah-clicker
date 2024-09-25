9b12a9e22b17cc1e3d5a0738d247649f6e5090c9
Alexander Eide <atlexeide@gmail.com>
2024-09-25 13:25:06 +0200

    Init js

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+const game = {
+    // White Monsters / Points    monsterAmount: undefined,
+    monsterAmount: 0,
+    // Clicks
+    clicks: 0,
+    // Upgrades
+    upgrades:{
+        programmerSocks:{
+            multiplier: undefined,
+        },
+        miku:{
+            multiplier: undefined,
+        },
+        neeko:{
+            multiplier: undefined,        
+        },
+    }
+}
