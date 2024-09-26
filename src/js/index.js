4b6a23f095802d7acfc666b7f53d7816ba0876f6
Alexander Eide <atlexeide@gmail.com>
2024-09-27 00:26:42 +0200

    Refactor upgrade logic

--- src/js/index.js
+++ src/js/index.js
@@ -19,6 +19,8 @@ const game = {
         let multiplier;
 
         for(const upgrade of game.activeUpgrades){
+            if(upgrade === undefined)
+                return
             console.log(game.activeUpgrades)
             multipliers.push(upgrade.multiplier)
         }
@@ -35,68 +37,82 @@ const game = {
         game.getMonstersLeft()
     },
     // // White Monsters / Points    monsterAmount: undefined,
-    monsterAmount: 0,
+    monsterAmount: 90,
     // Clicks
     clicks: 0,
     // Upgrades
-    upgrades:[
-        {   
+    upgrades:{
+         programmerSocks:{   
             name: 'programmerSocks',
             price: 100,
             monstersLeftToBuy: 999,
             multiplier: 0.5,
             description:'lorem ipsum',
+            owned:0,
 
         },
-        {
+        miku:{
             name: 'miku',
             price: 500,
             monstersLeftToBuy: 999,
-            multiplier: undefined,
+            multiplier: 10,
             description:'lorem ipsum',
+            owned:0,
 
         },
-        {
+        neeko:{
             name: 'neeko',
             price: 1000,
             monstersLeftToBuy: 999,
             multiplier: undefined,        
             description:'lorem ipsum',
+            owned:0,
 
         },
-    ],
+    },
     activeUpgrades:[],
     addUpgrade:(upgrade)=>{
-        game.activeUpgrades.push(upgrade)
+
+game.activeUpgrades.push(game.upgrades[upgrade])
+
+
+
+      
         const list = document.getElementById('stats-upgrades-display')
         list.innerHTML = '';
         for(const upgrade of game.activeUpgrades){
+            if(upgrade === undefined)
+                return
             const li = document.createElement('li');
             li.textContent = upgrade.name;
             list.appendChild(li)
         }
     },
     getMonstersLeft: ()=>{
-        for(const upgrade of game.upgrades){
-            upgrade.monstersLeftToBuy = upgrade.price - game.monsterAmount
-            document.getElementById(`${upgrade.name}-price`).textContent = `Price: ${upgrade.price} >> Monsters remaining to buy: ${upgrade.monstersLeftToBuy.toFixed(1)}`
+        for(const upgrade of Object.entries(game.upgrades)){
+            const item = upgrade[1];
+            item.monstersLeftToBuy = item.price - game.monsterAmount;
+            const priceElement = document.getElementById(`${item.name}-price`);
+            priceElement.textContent = `Price: ${item.price} >> Monsters remaining to buy: ${item.monstersLeftToBuy.toFixed(1)}`;
         }
     }
 }
 
 
 // ///////TESTING//////////
-document.querySelector('#programmerSocks-buy').addEventListener('click',()=>{
-    if(game.monsterAmount >= game.upgrades[0].price){
-        game.addUpgrade(game.upgrades[0]);
-        game.monsterAmount -= game.upgrades[0].price
+const programmerSocksBtn = document.querySelector('#programmerSocks-buy');
+programmerSocksBtn.addEventListener('click',()=>{
+   const upgrade = programmerSocksBtn.id.split('-')[0];
+    if(game.monsterAmount >= game.upgrades[upgrade].price){
+        game.addUpgrade(upgrade);
+        game.monsterAmount -= game.upgrades[upgrade].price
         game.updateDisplay()
     }
 })
 // /////////////////////////
 
 
-game.addUpgrade(game.upgrades[0])
+game.addUpgrade('miku')
 document.querySelector('#leah').addEventListener('mousedown',(e)=>{
 e.preventDefault()
 console.clear()
